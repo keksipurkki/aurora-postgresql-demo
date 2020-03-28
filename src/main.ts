d3190fa31d67287b97077a7ab2c0514ee92536df
Elias Toivanen <elias.a.toivanen@gmail.com>
2020-03-28 23:56:34 +0200

    Bastion host and networking fixes

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,22 @@ import * as secrets from "@aws-cdk/aws-secretsmanager";
 import * as assert from "assert";
 import * as rds from "@aws-cdk/aws-rds";
 
-const id = (name: string) => `aurora-demo-${name}`;
+const tag = (name: string) => `aurora-demo-${name}`;
 
 class DatabaseStack extends cdk.Stack {
+  vpc: ec2.IVpc;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.vpc = ec2.Vpc.fromLookup(this, tag("vpc"), {
+      isDefault: true,
+    });
   }
 
   cluster(name: string): rds.DatabaseCluster {
-    const vpc = ec2.Vpc.fromLookup(this, id("vpc"), {
-      isDefault: true,
-    });
-
     const cluster = new rds.DatabaseCluster(this, name, {
       engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      port: 5432,
       parameterGroup: this.parameterGroup,
       kmsKey: this.kmsKey,
       masterUser: {
@@ -29,7 +31,11 @@ class DatabaseStack extends cdk.Stack {
       instances: 2,
       instanceProps: {
         instanceType: this.instanceType,
-        vpc,
+        securityGroup: this.securityGroup,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        vpc: this.vpc,
       },
     });
 
@@ -37,56 +43,87 @@ class DatabaseStack extends cdk.Stack {
     const secret = cluster.node.findChild("Secret").node.defaultChild as secrets.CfnSecret;
     assert(secret instanceof secrets.CfnSecret);
 
-    secret.name = id("credentials");
+    secret.name = tag("credentials");
     secret.description = `Database credentials for ${this.stackName}`;
 
     return cluster;
   }
 
+  addBastionHost() {
+    const bastion = new ec2.BastionHostLinux(this, tag("bastion"), {
+      vpc: this.vpc,
+      instanceType: new ec2.InstanceType("t3.nano"),
+      instanceName: tag("bastion"),
+    });
+
+    bastion.allowSshAccessFrom(ec2.Peer.anyIpv4());
+    bastion.instance.instance.keyName = `aurora-demo-bastion`;
+
+    return bastion;
+  }
+
   get instanceType() {
-    return new ec2.InstanceType("t2.medium"); // Smallest possible
+    return new ec2.InstanceType("t3.medium"); // Smallest possible
   }
 
   get parameterGroup() {
     return rds.ParameterGroup.fromParameterGroupName(
       this,
-      id("parameter-group"),
+      tag("parameter-group"),
       `default.aurora-postgresql10`
     );
   }
 
   get kmsKey() {
-    return new kms.Key(this, id("key"), {
-      alias: `alias/${id("key")}`,
+    return new kms.Key(this, tag("key"), {
+      alias: `alias/${tag("key")}`,
     });
   }
 
-  associate(domainName: string, cluster: rds.DatabaseCluster) {
+  get securityGroup() {
+    const name = tag("sg");
 
-    const zone = route53.HostedZone.fromLookup(this, id('dns'), {
-      domainName: 'local.',
+    const sg = new ec2.SecurityGroup(this, name, {
+      vpc: this.vpc,
+      securityGroupName: name,
+    });
+
+    sg.connections.allowFrom(
+      ec2.Peer.ipv4(this.vpc.vpcCidrBlock),
+      ec2.Port.tcp(5432),
+      `Access to the database`
+    );
+
+    return sg;
+  }
+
+  associate(domainName: string, cluster: rds.DatabaseCluster) {
+    const zone = route53.HostedZone.fromLookup(this, tag("dns"), {
+      domainName: "keksipurkki.net",
+      privateZone: false,
     });
 
     return new route53.CnameRecord(this, domainName, {
       zone,
       domainName: cluster.clusterEndpoint.hostname,
-      recordName: domainName
+      recordName: domainName,
     });
   }
 }
 
 function main() {
   const env = { region: "eu-north-1", account: "011252223791" };
-  const stackName = id('stack');
-  const domainName = `${stackName}.local`;
+  const stackName = tag("stack");
+  const domainName = `aurora`; // aurora.keksipurkki.net
 
   const app = new cdk.App();
   const stack = new DatabaseStack(app, stackName, {
     env,
   });
 
-  const cluster = stack.cluster(id("cluster"));
+  const cluster = stack.cluster(tag("cluster"));
   stack.associate(domainName, cluster);
+  stack.addBastionHost();
   app.synth();
 }
 
